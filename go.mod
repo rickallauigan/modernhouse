7bc5683e63a77fe56c26b7645463c346c0956a87
RICK ALLAUIGAN <allauigan.rick@gmail.com>
2020-04-18 00:34:04 +0800

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module modernhouse
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.2
+require (
+	github.com/gin-contrib/cache v1.1.0
+	github.com/gin-gonic/gin v1.6.2
+)
--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cache"
+	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("public/*.html")
+	pageStore := persistence.NewInMemoryStore(time.Minute)
 
-	r.GET("/", func(c *gin.Context) {
+
+	r.GET("/", cache.CachePage(pageStore, time.Minute,func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
-	})
+	}))
 
 	r.Static("/public", "./public")
 
